89da7af552a78bfc00cb1d6333e24a60d017d243
Robert Manders <github.powdered589@passmail.net>
2025-12-07 12:44:12 +0000

    Update footer link.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from './Footer.module.css'
 export default function Footer() {
 	return (
 		<footer className={styles.footer}>
-			<a className={styles.footerLink} href='https://www.github.com/Rob-Manders/currency-calculator'>Github</a>
+			<a className={styles.footerLink} href='https://github.com/Rob-Manders/currency-converter'>Github</a>
 			<a className={styles.footerLink} href='https://currencybeacon.com/api-documentation'>Currency Beacon API</a>
 		</footer>
 	)
