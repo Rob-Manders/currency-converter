0b709cbbf3472fc6248a0b808926ff52b05bfbf9
Rob <rob@fedora.fritz.box>
2025-12-18 11:39:00 +0000

    Fix import

--- src/context/HistoryContext.tsx
+++ src/context/HistoryContext.tsx
@@ -1,5 +1,5 @@
 import { createContext } from 'react'
-import { Conversion, History } from '../history/history'
+import { Conversion, History } from '../history/history.ts'
 
 export const HistoryContext = createContext<any>({})
 
